19822fd3a43885da63af8336fe508d245c32bfd2
Omoraka Benjamin <omorakabenjamin3@gmail.com>
2025-09-23 12:21:44 +0000

    remove the component after header it should be this guy Find Nearest Hos

--- src/app/components/main-layout-client.tsx
+++ src/app/components/main-layout-client.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { EmergencyBanner } from "@/app/components/emergency-banner";
 import { Footer } from "@/app/components/layout/footer";
 import { Header } from "@/app/components/layout/header";
 
@@ -12,7 +11,6 @@ export function MainLayoutClient({
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <EmergencyBanner />
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
