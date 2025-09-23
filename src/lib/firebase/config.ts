816551efa09ef63f9be3ba1ad4fd68dfeb6d4382
Omoraka Benjamin <omorakabenjamin3@gmail.com>
2025-09-23 00:40:53 +0000

    Registration Failed Firebase:Error (auth/configuration-not-found)

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAoAz5EDSgmFMGL4Gvubu6sRQCZa8t7k_c",
+  authDomain: "studio-4884956211-8d085.firebaseapp.com",
+  projectId: "studio-4884956211-8d085",
+  storageBucket: "studio-4884956211-8d085.appspot.com",
+  messagingSenderId: "360937385656",
+  appId: "1:360937385656:web:e64b197b8160cb1e320e4d"
 };
 
 // Initialize Firebase
